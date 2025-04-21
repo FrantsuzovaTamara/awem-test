9d18c3dd4c68a2a8f03010d40058c25081aa66ce
Tamara Frantsuzova <t.f.66625262@yandex.ru>
2025-04-21 20:01:55 +0300

    Pixi Application created

---
+++ src/app/services/device.service.ts
@@ -0,0 +1,22 @@
+import {Position} from '../types'
+
+export class DeviceService {
+  public readonly WINDOW_MAX_WIDTH: number = 1400
+  public readonly WINDOW_MAX_HEIGHT: number = 1400
+
+  constructor() {}
+
+  public getDevicePixelRatio(): number {
+    return window.devicePixelRatio || 1
+  }
+
+  public getWindowWidth(): number {
+    return window.innerWidth
+  }
+
+  public getWindowHeight(): number {
+    return window.innerHeight
+  }
+}
+
+export default DeviceService
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import {createDiRootContainer} from './app/di/di.root-container'
+import { TOKENS } from './app/di/di.tokens'
+import './global.css'
 
+async function startApp($root: HTMLElement): Promise<void> {
+  console.debug('[🏁] Start', $root)
+  const container = createDiRootContainer($root)
+  const game = container.get(TOKENS.game)
+  await game.start()
+}
+
+const appEl = document.getElementById('app')
+if (!appEl) throw new Error(`appEl="#app" not found`)
+
+startApp(appEl).then(() => console.debug('[🏁] Finish')).catch(error => console.error(error))
