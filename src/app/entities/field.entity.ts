78f79dffd75753e20351effbd2e53c192ef2d6ef
Tamara Frantsuzova <t.f.66625262@yandex.ru>
2025-04-26 15:13:12 +0300

    Added animation for dragging chips

--- src/app/entities/field.entity.ts
+++ src/app/entities/field.entity.ts
@@ -23,6 +23,9 @@ export class FieldEntity {
   private readonly chips: ChipEntity[] = []
   private readonly sound: HTMLAudioElement
 
+  private activeChip: ChipEntity | undefined = undefined
+  private startDragPosition: Position | undefined = undefined
+
   constructor(private readonly createChip: () => ChipEntity) {
     this.container = new Container()
     this.chipsContainer = new Container()
@@ -40,11 +43,26 @@ export class FieldEntity {
     const x = window.innerWidth / 2
     const y = window.innerHeight / 2
 
-    this.chipTypes.forEach((chipName, i) => {
+    this.chipTypes.forEach((name, i) => {
       const chip = this.createChip()
 
-      const chipPosition = this.getChipPosition(x, y, i)
-      chip.init(chipName, chipPosition, this.chipsContainer)
+      const row = Math.floor(i / 3)
+      const column = i % 3
+      const position = this.getChipPosition(x, y, row, column)
+
+      chip.init({
+        name,
+        row,
+        column,
+        position,
+        container: this.chipsContainer,
+        onPointerDown: (e: PointerEvent) => {
+          this.activeChip = chip
+          this.startDragPosition = {x: e.clientX, y: e.clientY}
+        },
+        onPointerMove: this.onPointerMove,
+        cleanupDrag: this.cleanupDrag,
+      })
 
       this.chips.push(chip)
     })
@@ -84,11 +102,116 @@ export class FieldEntity {
     requestAnimationFrame(frame)
   }
 
-  private getChipPosition(x: number, y: number, chipIndex: number): Position {
-    const rowNumb = Math.floor(chipIndex / 3)
-    const columnNumb = chipIndex % 3
+  private readonly cleanupDrag = (): void => {
+    this.activeChip = undefined
+    this.startDragPosition = undefined
+    window.removeEventListener('pointermove', this.onPointerMove)
+    window.removeEventListener('pointerup', this.cleanupDrag)
+  }
+
+  private readonly onPointerMove = (e: PointerEvent): void => {
+    if (!this.activeChip || !this.startDragPosition) return
+
+    const dx = e.clientX - this.startDragPosition.x
+    const dy = e.clientY - this.startDragPosition.y
+
+    const distance = Math.hypot(dx, dy)
+    if (distance < 30) return
+
+    const dir = Math.abs(dx) > Math.abs(dy) ? (dx > 0 ? 'right' : 'left') : dy > 0 ? 'down' : 'up'
+
+    const neighbor = this.findNeighbor(dir)
+    if (neighbor) this.swapChipsAnimated(this.activeChip, neighbor)
+
+    this.cleanupDrag()
+  }
+
+  private findNeighbor(dir: 'left' | 'right' | 'up' | 'down'): ChipEntity | undefined {
+    if (!this.activeChip) return
+
+    for (const chip of this.chips) {
+      switch (dir) {
+        case 'left':
+          if (chip.column + 1 === this.activeChip.column && chip.row === this.activeChip.row) {
+            return chip
+          }
+          break
+        case 'right':
+          if (chip.column - 1 === this.activeChip.column && chip.row === this.activeChip.row) {
+            return chip
+          }
+          break
+        case 'up':
+          if (chip.column === this.activeChip.column && chip.row + 1 === this.activeChip.row) {
+            return chip
+          }
+          break
+        case 'down':
+          if (chip.column === this.activeChip.column && chip.row - 1 === this.activeChip.row) {
+            return chip
+          }
+          break
+      }
+    }
+    return
+  }
+
+  private async swapChipsAnimated(firstChip: ChipEntity, secondChip: ChipEntity): Promise<void> {
+    this.chips.forEach(chip => chip.lock())
+    const firstPos = {x: firstChip.x, y: firstChip.y}
+    const secondPos = {x: secondChip.x, y: secondChip.y}
+    const duration = 20
+    this.sound.play()
+
+    await this.swap(firstChip, secondChip, secondPos, firstPos, duration)
+
+    await new Promise(resolve => setTimeout(resolve, duration))
+
+    await this.swap(firstChip, secondChip, firstPos, secondPos, duration)
+  }
+
+  private swap(
+    firstChip: ChipEntity,
+    secondChip: ChipEntity,
+    firstTo: Position,
+    secondTo: Position,
+    duration: number,
+  ): Promise<void> {
+    return new Promise(resolve => {
+      const startFirstX = firstChip.x
+      const startFirstY = firstChip.y
+      const deltaFirstX = firstTo.x - startFirstX
+      const deltaFirstY = firstTo.y - startFirstY
+
+      const startSecondX = secondChip.x
+      const startSecondY = secondChip.y
+      const deltaSecondX = secondTo.x - startSecondX
+      const deltaSecondY = secondTo.y - startSecondY
+
+      let elapsed = 0
+
+      const frame = (): void => {
+        elapsed++
+
+        const progress = Math.min(elapsed / duration, 1)
+        firstChip.setPosition({x: startFirstX + deltaFirstX * progress, y: startFirstY + deltaFirstY * progress})
+        secondChip.setPosition({x: startSecondX + deltaSecondX * progress, y: startSecondY + deltaSecondY * progress})
+
+        if (progress < 1) {
+          requestAnimationFrame(frame)
+        } else {
+          this.chips.forEach(chip => chip.unlock())
+          resolve()
+        }
+      }
+
+      requestAnimationFrame(frame)
+    })
+  }
+
+  private getChipPosition(x: number, y: number, row: number, column: number): Position {
     const squareSide = 75
-    switch (rowNumb) {
+    switch (row) {
       case 0:
         y -= squareSide
         break
@@ -97,7 +220,7 @@ export class FieldEntity {
         break
     }
 
-    switch (columnNumb) {
+    switch (column) {
       case 0:
         x -= squareSide
         break
